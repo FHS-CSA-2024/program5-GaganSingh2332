7199d6df090fbdf063ae9c5b1c231050a10ac6fe
GaganSingh2332 <gagans0422@gmail.com>
2024-08-20 14:58:42 -0700

    Update Program5.java
    Finished Program5

--- Program5.java
+++ Program5.java
@@ -1,9 +1,52 @@
-//import stuff here?
 
+
+//import stuff here?
+public class Program5 {
+    public static void main(String[] args) {
+    double royale = (double) 286 / 9;
+    royale += 0.05;
+    int royale1 = (int) (royale * 10);
+    royale =  (double) royale1 / 10;
+    
+    double koopaking = (double) 412/40;
+    koopaking += 0.05;
+    int koopaking1 = (int) (koopaking * 10);
+    koopaking = (double) koopaking1 /10;
+    
+    double pipeframe = (double) 361/18;
+    pipeframe += 0.05;
+    int pipeframe1 = (int) (pipeframe * 10);
+    pipeframe = (double) pipeframe1 / 10;
+    
+    double badwagon = (double) 161/11;
+    badwagon += 0.05;
+    int badwagon1 = (int) (badwagon * 10);
+    badwagon = (double) badwagon1 / 10;
+    
+    
+    System.out.println("Mushroom Cup Racer Average Miles/Per Gallon:");
+    System.out.println("Royale averaged " + royale + " m/g" );
+    System.out.println("Koopa King averaged " + koopaking + " m/g");
+    System.out.println("Pipe Frame averaged " + pipeframe +  " m/g");
+    System.out.println("Badwagon averaged " + badwagon + " m/g");
+    
+    
+    
+    
+    }
+}
 //Your code here
 
 //Paste console output below:
 /*
+Mushroom Cup Prix Racer Average Miles/Per Gallon:
 
+Royale averaged 31.8 m/g
+
+Koopa King averaged 10.3 m/g
+
+Pipe Frame averaged 20.1 m/g
+
+Badwagon averaged 14.6 m/g
 
 */
